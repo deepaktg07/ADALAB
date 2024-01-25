82557c97fb8f8885a71f015ca2d2b1a680364d55
Deepak <120008495+deepaktg07@users.noreply.github.com>
2024-01-25 08:44:42 +0530

    Update PRIMS.cpp

--- PRIMS.cpp
+++ PRIMS.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
-const int MAX = 20;
-int n, c[MAX][MAX], i, j, visited[MAX];
+int n, c[20][20], i, j, visited[20];
 void prim();
 int main() {
     cout << "Enter number of vertices" << endl;
@@ -17,28 +16,21 @@ int main() {
 }
 void prim() {
     int min, a, b, k, count = 0, cost = 0;
+    min = 999;
     visited[1] = 1;
     while (count < n - 1) {
         min = 999;
-        for (i = 1; i <= n; i++) {
-            if (visited[i]) {
-                for (j = 1; j <= n; j++) {
-                    if (!visited[j] && min > c[i][j]) {
-                        min = c[i][j];
-                        a = i;
-                        b = j;
-                    }
+        for (i = 1; i <= n; i++)
+            for (j = 1; j <= n; j++)
+                if (visited[i] && !visited[j] && min > c[i][j]) {
+                    min = c[i][j];
+                    a = i;
+                    b = j;
                 }
-            }
-        }
-        if (min == 999) {
-            cout << "Graph is not connected." << endl;
-            return;
-        }
-        cout << a << "--->" << b << " = " << min << endl;
-        cost += min;
+        cout << a << "--->" << b << " = " << c[a][b] << endl;
+        cost += c[a][b];
         visited[b] = 1;
         count++;
     }
-    cout << "Total cost of Spanning tree is " << cost << endl;
+    cout << "Total cost of Spanning tree is " << cost;
 }
