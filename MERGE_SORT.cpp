511dc7fdec10315a5226b286ba78d1f92b919ac8
Deepak <deepakgonchikar0707@gmail.com>
2024-03-12 22:53:07 +0530

    Update MERGE_SORT.cpp

--- MERGE_SORT.cpp
+++ MERGE_SORT.cpp
@@ -1,61 +1,59 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-void Merge(int arr[], int low, int high, int mid) {
-    int i, j, k;
-    int temp[high - low + 1];
-    i = low;
-    k = 0;
-    j = mid + 1;
-    while (i <= mid && j <= high) {
-        if (arr[i] < arr[j]) {
-            temp[k] = arr[i];
+void merge(int arr[],int low,int high,int mid){
+    int temp[high-low+1];
+    int i=low;
+    int k=0;
+    int j=mid+1;
+    while(i<=mid && j<=high){
+        if(arr[i]<arr[j]){
+            temp[k]=arr[i];
             k++;
             i++;
-        } else {
-            temp[k] = arr[j];
+        }
+        else{
+            temp[k]=arr[j];
             k++;
             j++;
         }
     }
-    while (i <= mid) {
-        temp[k] = arr[i];
-        k++;
-        i++;
+    while(i<=mid){
+        temp[k]=arr[i];
+            k++;
+            i++;
     }
-    while (j <= high) {
-        temp[k] = arr[j];
-        k++;
-        j++;
+    while(j<=high){
+        temp[k]=arr[j];
+            k++;
+            j++;
     }
-    for (i = low; i <= high; i++) {
-        arr[i] = temp[i - low];
+    for(int i=low;i<=high;i++){
+        arr[i]=temp[i-low];
     }
 }
-
-void MergeSort(int arr[], int low, int high) {
+void mergesort(int arr[],int low,int high){
     int mid;
-    if (low < high) {
-        mid = (low + high) / 2;
-        MergeSort(arr, low, mid);
-        MergeSort(arr, mid + 1, high);
-        Merge(arr, low, high, mid);
+    if(low<high){
+        mid=(low+high)/2;
+        mergesort(arr,low,mid);
+        mergesort(arr,mid+1,high);
+        merge(arr,low,high,mid);
     }
 }
-
 int main() {
     int n, i;
     cout << "Enter the number of data elements to be sorted: ";
     cin >> n;
     int arr[n];
+    cout<<"Enter the elements: "<<endl;
     for (i = 0; i < n; i++) {
-        cout << "Enter element " << i + 1 << ": ";
         cin >> arr[i];
     }
-    MergeSort(arr, 0, n - 1);
+    mergesort(arr, 0, n - 1);
     cout << "\nSorted Data: ";
     for (i = 0; i < n; i++) {
-        cout << "->" << arr[i];
+        cout << arr[i]<<"\t";
     }
     return 0;
 }
