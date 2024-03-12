9553c159d252ed253d9359ed3c3291ddd9ca26fa
Deepak <deepakgonchikar0707@gmail.com>
2024-03-12 18:48:01 +0530

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -2,46 +2,44 @@
 #include <stack>
 using namespace std;
 
-int n, i, j, visited[10];
-int adj[10][10];
+int n, adj[10][10], visited[10];
 
 void dfs(int v) {
     stack<int> st;
-    st.push(v); // Push the starting vertex onto the stack
+    st.push(v);
+    visited[v] = 1;
 
     while (!st.empty()) {
-        v = st.top(); // Get the top element from the stack
-        st.pop();     // Pop the top element
+        int currentVertex = st.top();
+        st.pop();
+        cout << currentVertex << " ";
 
-        if (!visited[v]) {
-            cout << v << " "; // Print the current vertex
-            visited[v] = 1;   // Mark the current vertex as visited
-
-            // Push unvisited adjacent vertices onto the stack
-            for (int i = n; i >= 1; i--) {
-                if (adj[v][i] && !visited[i]) {
-                    st.push(i);
-                }
+        for (int i = 1; i <= n; ++i) {
+            if (adj[currentVertex][i] && !visited[i]) {
+                st.push(i);
+                visited[i] = 1;
             }
         }
     }
 }
+
 int main() {
     int v;
     cout << "Enter the number of vertices: ";
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        visited[i] = 0; // Initialize visited array
-    }
-    cout << "Enter graph data in matrix form:\n";
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            cin >> adj[i][j]; // Input adjacency matrix
+
+    cout << "Enter the adjacency matrix: \n";
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            cin >> adj[i][j];
         }
     }
     cout << "Enter the starting vertex for DFS: ";
     cin >> v;
+
     cout << "DFS traversal starting from vertex " << v << " is: ";
-    dfs(v); // Call DFS function
+    dfs(v);
+    cout << endl;
+
     return 0;
 }
